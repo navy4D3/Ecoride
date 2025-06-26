50e9cfd0837c27d1b58d10b09e8479d515b7c2ef
navy <yvanrobert974@gmail.com>
2025-06-26 18:53:33 +0200

    integration recap add-trajet

--- assets/js/addTrajet.js
+++ assets/js/addTrajet.js
@@ -20,6 +20,7 @@ const minutesSelect = document.getElementById('minutes');
 const heureDepartInput = document.getElementById('add_trajet_heureDepart');
 const dateDepartInput = document.getElementById('add_trajet_dateDepart');
 
+
   // Remplir les heures
 for (let h = 0; h < 24; h++) {
 const option = document.createElement('option');
@@ -113,7 +114,8 @@ prixInput.addEventListener('input', function() {
   }
 });
 
-const secondSection = document.getElementById('itineraires-choices');
+const itinerairesSection = document.getElementById('itineraires-choices');
+const confirmItineraireBtn = document.getElementById('confirm-itineraire-btn');
 
 function rechercherItineraire(origin, destination) {
     fetch(`/publier-trajet/itineraires?origin=${encodeURIComponent(origin)}&destination=${encodeURIComponent(destination)}`)
@@ -140,7 +142,7 @@ function rechercherItineraire(origin, destination) {
         
         
         addTrajetForm.style.display = "none";
-        secondSection.style.display = "flex";
+        itinerairesSection.style.display = "flex";
         document.querySelector('h1').innerHTML = "Choisis un itinéraire";
 
         document.querySelectorAll('.itineraire').forEach(function(itineraire) {
@@ -150,6 +152,9 @@ function rechercherItineraire(origin, destination) {
                 const googleDataInput = document.getElementById('add_trajet_googleData');
 
                 googleDataInput.value = this.dataset.raw;
+                
+                
+                confirmItineraireBtn.classList.remove('inactive');
                 
             })
         })
@@ -166,7 +171,7 @@ const continueBtn = document.getElementById('continue-btn');
 const departInput = document.getElementById('add_trajet_lieuDepart');
 const arriveeInput = document.getElementById('add_trajet_lieuArrivee');
 
-const backBtn = document.getElementById('back-btn');
+const itinerairesBackBtn = itinerairesSection.querySelector('.back-btn');
 
 continueBtn.addEventListener('click', function(e) {
     e.preventDefault();
@@ -178,10 +183,18 @@ continueBtn.addEventListener('click', function(e) {
 
 })
 
-backBtn.addEventListener('click', function() {
+itinerairesBackBtn.addEventListener('click', function() {
     addTrajetForm.style.display = "flex";
-    secondSection.style.display = "none";
+    itinerairesSection.style.display = "none";
     document.querySelector('h1').innerHTML = "Ajouter un trajet";
+
+    confirmItineraireBtn.classList.add('inactive');
+    const currentItinerairesDiv =  document.querySelectorAll('.itineraire');
+
+    currentItinerairesDiv.forEach((div) => {
+      div.remove();
+    });
+
 })
 
 const addVoitureBtn = document.getElementById("add-voiture-btn");
@@ -191,42 +204,101 @@ addVoitureBtn.addEventListener('click', function() {
 
 });
 
-
-// export function initUserVoitureBtns() {
-//   const userVoituresBtns = document.querySelectorAll(".voiture-card");
-//   const voitureInput =  document.querySelector('voiture-input');
-
-//   userVoituresBtns.forEach(voitureBtn => {
-//     voitureBtn.addEventListener('click', function(e) {
-//       const currentVoitureId = voitureBtn.id;
-//       userVoituresBtns.forEach(voitureBtn => voitureBtn.classList.remove('selected'));
-//       this.classList.add('selected');
-//       voitureInput.value = currentVoitureId;
-//       //declenche event sur voiture manuellement
-//       const event = new Event("input", { bubbles: true });
-//       voitureInput.dispatchEvent(event);
-
-//     })
-//   })
-// }
-
 initUserVoitureBtns();
 
-// addVoitureBtn.addEventListener('click', function() {
+const recapitulatifSection = document.getElementById('recapitulatif');
+const recapitulatifBackBtn = recapitulatifSection.querySelector('.back-btn');
+
+confirmItineraireBtn.addEventListener('click', function() {
+  document.querySelector('h1').innerHTML = "Récapitulatif";
+  recapitulatifSection.style.display = "flex";
+  itinerairesSection.style.display = "none";
+
+  const lieuDepartInput = document.getElementById('add_trajet_lieuDepart');
+  const lieuArriveeInput = document.getElementById('add_trajet_lieuArrivee');
+  const prixPassagerInput = document.getElementById('add_trajet_prixPersonne');
+
+  const recapDateDepartLabel = recapitulatifSection.querySelector('.date-depart');
+  const recaplieuDepartLabel = recapitulatifSection.querySelector('.lieu-depart');
+  const recapHeureDepartLabel = recapitulatifSection.querySelector('.heure-depart');
+  const recapDureeLabel = recapitulatifSection.querySelector('.duree');
+  const recapDistanceLabel = recapitulatifSection.querySelector('.distance');
+  const recapheureArriveeLabel = recapitulatifSection.querySelector('.heure-arrivee');
+  const recaplieuArriveeLabel = recapitulatifSection.querySelector('.lieu-arrivee');
+
+  const recapVoitureSurnomLabel = recapitulatifSection.querySelector('.voiture-surnom');
+  const recapVoitureMarqueLabel = recapitulatifSection.querySelector('.voiture-marque');
+  const recapVoitureModeleLabel = recapitulatifSection.querySelector('.voiture-modele');
+  const recapVoitureIsElectricLabel = recapitulatifSection.querySelector('.voiture-is-electric');
+  const recapVoiturePlacesLabel = recapitulatifSection.querySelector('.voiture-place');
+  const recapVoitureCreditsLabel = recapitulatifSection.querySelector('.credits-passager');
+
+  recapDateDepartLabel.innerHTML = "Départ le " + dateDepartInput.value;
+  recaplieuDepartLabel.innerHTML = lieuDepartInput.value;
+  recapHeureDepartLabel.innerHTML = hoursSelect.value + ":" + minutesSelect.value;
+  recaplieuArriveeLabel.innerHTML = lieuArriveeInput.value;
+  recapVoitureCreditsLabel.innerHTML = prixPassagerInput.value + " crédits / passager";
+
+  const currentItineraireSelectedDiv =document.querySelector('.itineraire.selected');
+
+  const currentItineraireSelectedDatas = JSON.parse(currentItineraireSelectedDiv.dataset.raw);
   
-//   document.getElementById('trajet-details').style.display = "none";
-//   const addVoitureDiv = document.getElementById('add-voiture-section');
+  const currentItineraireSelectedDureeSeconds = currentItineraireSelectedDatas['legs'][0]['duration']['value'];
+  const currentItineraireSelectedDistanceText = currentItineraireSelectedDatas['legs'][0]['distance']['text'];
 
-//   if (addVoitureDiv) {
-//     addVoitureDiv.style.display = "flex";
-//     const addVoitureForm = document.getElementById('add-voiture-form');
-//     addVoitureForm.reset();
-//   } else {
-//     addVoiture('body-content', "trajet-details");
-    
-//   }
+  const currentItineraireSelectedDureeText = formatSecondsToHoursMinutes(currentItineraireSelectedDureeSeconds);
 
-// })
+  // recapheureArriveeLabel.innerHTML = 
+  recapDureeLabel.innerHTML = currentItineraireSelectedDureeText;
+  recapDistanceLabel.innerHTML = currentItineraireSelectedDistanceText;
+  recapheureArriveeLabel.innerHTML = calculerHeureArrivee(heureDepartInput.value, currentItineraireSelectedDureeSeconds);
+
+  
+
+  const currentVoitureSelectedDiv = document.querySelector('.voiture-card.selected');
+  const currentVoitureSelectedSurnom = currentVoitureSelectedDiv.querySelector('.surnom').innerHTML;
+  const currentVoitureSelectedMarque = currentVoitureSelectedDiv.querySelector('.marque').innerHTML;
+  const currentVoitureSelectedModele = currentVoitureSelectedDiv.querySelector('.modele').innerHTML;
+  const currentVoitureSelectedIsElectric = currentVoitureSelectedDiv.querySelector('.is-electric').innerHTML;
+  const currentVoitureSelectedPlaces = currentVoitureSelectedDiv.querySelector('.places').innerHTML;
+
+  recapVoitureSurnomLabel.innerHTML = currentVoitureSelectedSurnom;
+  recapVoitureMarqueLabel.innerHTML = currentVoitureSelectedMarque;
+  recapVoitureModeleLabel.innerHTML = currentVoitureSelectedModele;
+  recapVoitureIsElectricLabel.innerHTML = currentVoitureSelectedIsElectric;
+  recapVoiturePlacesLabel.innerHTML = currentVoitureSelectedPlaces;
+
+  function formatSecondsToHoursMinutes(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+
+    const h = hours.toString().padStart(2, '0');
+    const m = minutes.toString().padStart(2, '0');
+
+    return `${h}:${m}`;
+  }
+
+  function calculerHeureArrivee(departIso, dureeSecondes) {
+    const dateDepart = new Date(departIso);
+    const dateArrivee = new Date(dateDepart.getTime() + dureeSecondes * 1000);
+
+    const heures = dateArrivee.getHours().toString().padStart(2, '0');
+    const minutes = dateArrivee.getMinutes().toString().padStart(2, '0');
+
+    return `${heures}h${minutes}`;
+}
+
+
+})
+
+recapitulatifBackBtn.addEventListener('click', function() {
+  recapitulatifSection.style.display = "none";
+  itinerairesSection.style.display = "flex";
+})
+
+
+
+
 
 
 
